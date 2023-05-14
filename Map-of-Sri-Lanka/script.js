da356b5bbb5097b210aa1c7cbe39982f8d9adbf3
Hicksy <hicks.harryj@gmail.com>
2023-05-14 15:12:13 +0100

    Delete Map-of-Sri-Lanka directory

--- Map-of-Sri-Lanka/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $('.svg-menu__path__seleccion').on("click", function(e){
-    $('.active').removeClass('active');
-    $(this).addClass('active');
-    //alert('evento');
-  });
-});
